c0ed6c022529b5e6552897071c9860ac5639cd46
Huck-Richardson <111768816+Huck-Richardson@users.noreply.github.com>
2023-03-21 19:55:22 -0400

    Add files via upload

--- upc.cpp
+++ upc.cpp
@@ -1,7 +1,7 @@
 # include <iostream>
 # include <cmath>
 
-int main () {
+int main() {
 
 //Declaring Varibles
 double v;
@@ -17,33 +17,82 @@ double v_mph;
 double power;
 double mass;
 double air_density;
+double u_input;
+double grade = 8;
 
+if (grade == 9) {
+ 
+  std::cout << "Freshman\n";
+ 
+}
 //Title Screen
-std::cout << "#############################################\n";
-std::cout << "#                                           #\n";
-std::cout << "#  ULTIMATE PROJECTILE CALCULATOR IMPROVED  #\n";
-std::cout << "#            BY HUCK-RICHARDSON             #\n";
-std::cout << "#                                      v0.6 #\n";
-std::cout << "#############################################\n";
+std::cout << "#############################################################\n";
+std::cout << "#                                                           #\n";
+std::cout << "#           ULTIMATE PROJECTILE CALCULATOR IMPROVED         #\n";
+std::cout << "#                     BY HUCK-RICHARDSON                    #\n";
+std::cout << "#                                                           #\n";
+std::cout << "#                  1:Scientest     2:Normal                 #\n";
+std::cout << "#                                                      v0.7 #\n";
+std::cout << "#############################################################\n";
+std::cin >> u_input;
 
-//Collecting Infomation
-//Person Info
-std::cout << "Please Enter your height (m)\n";
-std::cin >> height;
-std::cout << "Please Enter your weight (kg)\n";
-std::cin >> weight;
-std::cout << "Please enter your power output (w/kg)\n";
-std::cin >> power;
-//Projectile Info
-std::cout << "Please Enter the mass of the projectile (kg)\n";
-std::cin >> mass;
-std::cout << "Please Enter the projected area of the projectile (m2)\n";
-std::cin >> pro_area;
-//Planet info
-std::cout << "Please enter the gravitational acceleration (m/s)\n";
-std::cin >> g;
-std::cout << "Please enter enter the air density (kg/m3)\n";
-std::cin >> air_density;
+if ( u_input == 1 ) {
+//Scientest mode
+	//Collecting Infomation
+	//Person Info
+	std::cout << "Please enter your height (m)\n";
+	std::cin >> height;
+	std::cout << "Please enter your weight (kg)\n";
+	std::cin >> weight;
+	std::cout << "Please enter your power output (w/kg)\n";
+	std::cin >> power;
+	//Projectile Info
+	std::cout << "Please enter the mass of the projectile (kg)\n";
+	std::cin >> mass;
+	std::cout << "Please enter the projected area of the projectile (m2)\n";
+	std::cin >> pro_area;
+	//Planet info
+	std::cout << "Please enter the gravitational acceleration (m/s)\n";
+	std::cin >> g;
+	std::cout << "Please enter enter the air density (kg/m3)\n";
+	std::cin >> air_density;
+}
+
+if ( u_input == 2 ) {
+//Normal Mode
+	//Collecting Info
+	//Person Info
+	std::cout << "Please enter your height (m)\n";
+	std::cin >> height;
+	std::cout << "Please enter your weight (kg)\n";
+	std::cin >> weight;
+	std::cout << "Please choose one of the options below for physical ability\n";
+	std::cout << "###############\n";
+	std::cout << "# 1:Althete   #\n";
+	std::cout << "# 2:Normal    #\n";
+	std::cout << "# 3:Jelly Man #\n";
+	std::cout << "###############\n";
+	std::cin >> u_input;
+	if (u_input == 1) {
+ 		power = 20;	
+	}
+	if (u_input == 2) {
+		power = 10;
+	}
+	if (u_input == 3) {
+		power = 5;
+	}
+	
+	//Projectile Info
+	std::cout << "Please enter the mass of the projectile (kg)\n";
+	std::cin >> mass;
+	std::cout << "Please enter the projected area of the projectile (m2)\n";
+	std::cin >> pro_area;
+	// Asuming you live on earth and live in a normal area
+	g = 9.8;
+	air_density = 1;
+}
+
 
 //Calculations
 fudge_factor = weight / 1000;
